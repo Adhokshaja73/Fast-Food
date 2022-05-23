ad0d1de4a61860469eb30e991d7bbeee5e6d2da3
eva2468 <abhishek.ev@mca.christuniversity.in>
2022-05-23 13:43:10 +0530

    update manageorder

--- django_server/home/models.py
+++ django_server/home/models.py
@@ -1,7 +1,9 @@
+import imp
 from operator import mod
 from pyexpat import model
 from django.db import models
 from . import *
+from .constats import *
 # Create your models here.
 
 
@@ -61,18 +63,28 @@ class Order(models.Model):
 
 
 class Payments(models.Model):
-    PAYMENT_STATUS = (
-        (0, "New"),
-        (1, "Processing"),
-        (2, "Complete"),
-        (-1, "Cancelled")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField(("Amount"), null=False, blank=False)
+    
+    status = models.CharField(
+        ("Payment Status"),
+        default=PaymentStatus.PENDING,
+        max_length=254,
+        blank=False,
+        null=False,
     )
-    payment_id = models.CharField( max_length =256, primary_key=True) 
-    user = models.ForeignKey(User, on_delete=models.CASCADE) 
-    shop = models.ForeignKey(Shop , on_delete=models.CASCADE) 
-    amount = models.DecimalField( max_digits = 6, decimal_places=2) 
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    status = models.IntegerField(choices=PAYMENT_STATUS) 
+    provider_order_id = models.CharField(
+        ("Order ID"), max_length=40, null=False, blank=False
+    )
+    payment_id = models.CharField(
+        ("Payment ID"), max_length=36, null=False, blank=False
+    )
+    signature_id = models.CharField(
+        ("Signature ID"), max_length=128, null=False, blank=False
+    )
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.status}" 
 
 
 class FoodItem(models.Model):
